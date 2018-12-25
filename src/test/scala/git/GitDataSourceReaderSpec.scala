17870cb97d07344d2e2deab0bd9ad4b8d7e9e72c
Surya Gaddipati <sgaddipati@groupon.com>
2018-12-24 20:34:57 -0600

    fix example

--- src/test/scala/git/GitDataSourceReaderSpec.scala
+++ src/test/scala/git/GitDataSourceReaderSpec.scala
@@ -5,12 +5,11 @@ import org.scalatest._
 
 class GitDataSourceReaderSpec extends FlatSpec with Matchers {
   "GitDataSource" should "work with spark sql" in {
-    val logFile = "/Users/sgaddipati/code/clam/.git" // Should be some file on your system
+    val logFile = "/Users/sgaddipati/code/spark/.git" // Should be some file on your system
     val spark = SparkSession.builder.master("local[*]").appName("Git datasource").getOrCreate()
-
     val logData = spark.read.format("sg.spark.git").load(logFile)
-    logData.createOrReplaceTempView("giti")
-    val m = spark.sql("select * from giti")
+    logData.createOrReplaceTempView("repoName")
+    val m = spark.sql("select author.name, count(*)  from repoName group by author.name order by count(*) desc")
     m.show()
     spark.stop()
   }
