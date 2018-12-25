cf3984639af4eb9f0ce5afe665a2454832b09fb1
Surya Gaddipati <sgaddipati@groupon.com>
2018-12-24 19:51:48 -0600

    nest author

--- src/main/scala/git/GitDataSourceReader.scala
+++ src/main/scala/git/GitDataSourceReader.scala
@@ -3,33 +3,30 @@ package sg.spark.git
 import java.io.File
 import java.util
 
+import org.apache.spark.internal.Logging
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.sources.v2.DataSourceOptions
 import org.apache.spark.sql.sources.v2.reader.{DataSourceReader, InputPartition, InputPartitionReader}
 import org.apache.spark.sql.types._
 import org.apache.spark.unsafe.types.UTF8String
-import org.eclipse.jgit.api.{Git, LogCommand}
+import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.internal.storage.file.FileRepository
 import org.eclipse.jgit.revwalk.RevCommit
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder
 
 
-class GitDataSourceReader(options: DataSourceOptions) extends DataSourceReader  {
+class GitDataSourceReader(options: DataSourceOptions) extends DataSourceReader with Logging  {
 
   override def readSchema(): StructType =  StructType(
     StructField("shortMessage", DataTypes.StringType, true)::
-      StructField("author", DataTypes.StringType, true)
+     StructField("author",StructType(
+      StructField("name", DataTypes.StringType, true) :: Nil) )
       ::Nil)
 
   override def planInputPartitions():  util.List[InputPartition[InternalRow]] = {
-     val x = new util.ArrayList[GitInputPartition]()
-    x.add(new GitInputPartition(options.get(DataSourceOptions.PATH_KEY).get))
-    x.asInstanceOf[util.List[InputPartition[InternalRow]]]
+    util.Arrays.asList(new GitInputPartition(options.get(DataSourceOptions.PATH_KEY).get))
   }
 
-//  override def setOffsetRange(start: Optional[Offset], end: Optional[Offset]): Unit = {}
-
-//  override def getEndOffset: Offset = LongOffset(9l)
 }
  class GitInputPartition(path: String) extends  InputPartition[InternalRow]{
    override def createPartitionReader(): GitLogReader =  {
@@ -44,8 +41,10 @@ class GitDataSourceReader(options: DataSourceOptions) extends DataSourceReader  
 
    override def get(): InternalRow = {
       val revCommit = log.next()
-      InternalRow (UTF8String.fromString( revCommit.getShortMessage),UTF8String.fromString( revCommit.getAuthorIdent.getName))
+      InternalRow (str(revCommit.getShortMessage),
+       InternalRow( str( revCommit.getAuthorIdent.getName)))
    }
+   def str(string:String) = UTF8String.fromString(string)
 
    override def close(): Unit = {}
  }
--- src/test/scala/git/GitDataSourceReaderSpec.scala
+++ src/test/scala/git/GitDataSourceReaderSpec.scala
@@ -6,15 +6,12 @@ import org.scalatest._
 class GitDataSourceReaderSpec extends FlatSpec with Matchers {
   "GitDataSource" should "work with spark sql" in {
     val logFile = "/Users/sgaddipati/code/clam/.git" // Should be some file on your system
-    val spark = SparkSession.builder.master("local[*]").appName("Simple Application").getOrCreate()
+    val spark = SparkSession.builder.master("local[*]").appName("Git datasource").getOrCreate()
 
     val logData = spark.read.format("sg.spark.git").load(logFile)
     logData.createOrReplaceTempView("giti")
-    val m = spark.sql("select * from giti")
+    val m = spark.sql("select author.name from giti")
     m.show()
-    //  val numAs = logData.filter(line => line.contains("a")).count()
-    //  val numBs = logData.filter(line => line.contains("b")).count()
-    //  println(s"Lines with a: $numAs, Lines with b: $numBs")
     spark.stop()
   }
 }
